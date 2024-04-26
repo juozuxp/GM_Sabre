baf16db67be8be0c7dc6b7bd1ce907c9c0bb1cd7
void <juozuxp@gmail.com>
2024-04-27 02:52:21 +0300

    Set appropriate title for string view

--- Sabre/Views/StringView/StringViewView.cs
+++ Sabre/Views/StringView/StringViewView.cs
@@ -23,7 +23,7 @@ namespace Sabre.Views
         /// </summary>
         public StringViewView() : base(null)
         {
-            this.Caption = "StringView";
+            this.Caption = "String view";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
